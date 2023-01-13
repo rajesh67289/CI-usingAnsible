816b0b08fac3699ef813bab0a9d6a7b6e8645014
rajesh67289 <44958379+rajesh67289@users.noreply.github.com>
2023-01-13 12:42:51 +0530

    Update tomcat-systemd.sh

--- roles/tomcat/files/tomcat-systemd.sh
+++ roles/tomcat/files/tomcat-systemd.sh
@@ -7,12 +7,12 @@ Type=forking
 User=tomcat
 Group=tomcat
 
-Environment=CATALINA_PID=/opt/apache-tomcat-8.5.84/temp/tomcat.pid
-Environment=CATALINA_HOME=/opt/apache-tomcat-8.5.84
+Environment=CATALINA_PID=/opt/apache-tomcat-8.5.42/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/apache-tomcat-8.5.42
 Environment=CATALINA_BASE=/opt/apache-tomcat-8.5.84
 
-ExecStart=/opt/apache-tomcat-8.5.84/bin/catalina.sh start
-ExecStop=/opt/apache-tomcat-8.5.84/bin/catalina.sh stop
+ExecStart=/opt/apache-tomcat-8.5.42/bin/catalina.sh start
+ExecStop=/opt/apache-tomcat-8.5.42/bin/catalina.sh stop
 
 RestartSec=10
 Restart=always
